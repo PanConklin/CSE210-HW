556cbf592770b49394ff4b8303b980664bbc6b86
Brett Conklin <breadconklin@icloud.com>
2023-05-01 17:48:33 -0600

    Finished all tasks for Learning02

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,3 +1,5 @@
+using System;
+
 public class Job
 {
     private string _company;
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,7 +7,16 @@ class Program
         Job job1 = new Job("Sofware engineer", "Microsoft", 2019, 2022);
         Job job2 = new Job("Manger", "Apple", 2022, 2023);
 
-        job1.Display();
-        job2.Display();
+        //job1.Display();
+        //job2.Display();
+
+        Resume res = new Resume();
+        res._name = "Brett Conklin";
+
+        res._jobs.Add(job1);
+        res._jobs.Add(job2);
+
+        res.Display();
+
     }
 }
